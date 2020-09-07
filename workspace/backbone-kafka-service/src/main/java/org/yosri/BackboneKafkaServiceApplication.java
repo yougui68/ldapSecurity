7c95795e727c473a4607e9d654cff606a05e1d47
Naghmouchi <nyosri.am68@gmail.com>
2020-09-07 14:19:30 +0200

    first commit kafka

---
+++ workspace/backbone-kafka-service/src/main/java/org/yosri/BackboneKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package org.yosri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackboneKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackboneKafkaServiceApplication.class, args);
+	}
+
+}
